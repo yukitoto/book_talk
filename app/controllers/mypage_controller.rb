f7f93414069b0a9de7f39367d199c3f4f1943b40
Yukito Shibuya <shibuya@dotinstall.com>
2015-12-04 03:04:16 +0900

    Deployed view of mypage

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -2,6 +2,6 @@ class MypageController < ApplicationController
   before_action :authenticate_user!
   def home
     @user = current_user
-    @topics = Topic.where(user_id: current_user.id)
+    @topics = @user.topics.reorder('created_at DESC')
   end
 end
