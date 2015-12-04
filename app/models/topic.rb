76c72ba7ed32fffb3097920b0b357377d2dc5a36
Yukito Shibuya <shibuya@dotinstall.com>
2015-12-04 21:14:56 +0900

    Modified limit-number-word

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,5 @@ class Topic < ActiveRecord::Base
   belongs_to :user 
   has_many :comments
   validates :title,:detail,:book_title,:main_author,:user_id, presence:true
+  validates :title, length: { maximum: 30 }
 end
