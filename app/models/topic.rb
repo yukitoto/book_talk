bf129cee6bace3bf6bd0bcfabebc6143b7521029
Yukito Shibuya <shibuya@dotinstall.com>
2015-12-04 20:24:41 +0900

    Modified validation of topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,5 @@
 class Topic < ActiveRecord::Base
   belongs_to :user 
   has_many :comments
-  validates :title,:detail,:book_title,:main_author, presence:true
+  validates :title,:detail,:book_title,:main_author,:user_id, presence:true
 end
